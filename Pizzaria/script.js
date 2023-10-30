2135a561279a5522941c12215f727f0b569d12bf
Ivan Mascarenhas <ivan.mascarenhas@hp.com>
2023-10-29 22:59:02 -0300

    Adicionando controle de pedidos

--- Pizzaria/script.js
+++ Pizzaria/script.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('#form_pedido')
 const fieldsForm1 = ["nome", "telefone", "endereco", "sabor_pizza", "tamanho_pizza"];
-const checkboxes = document.querySelectorAll("#form_pedido input[type]")
 
 form.addEventListener('submit', (e) => cadastrarPizza(e, fieldsForm1))
 
@@ -12,17 +11,42 @@ function cadastrarPizza(eventoDeSubmit, fields) {
     fields.forEach(item => {
         const field = eventoDeSubmit.target[item].value
 
-        if(!field) {
+        if (!field) {
             formOk = false
         }
     })
 
-    
     if(!formOk) {
         alert("Preencha todos os campos")
         return;
     }
 
+    table = document.querySelector("#tabela_pedidos")
+    tr = table.insertRow()
+    hora = tr.insertCell()
+    hora.innerHTML = new Date(Date.now()).toLocaleString();
+    pegaValores(eventoDeSubmit.target,function (prop,value){
+        const cell = tr.insertCell()
+        cell.innerHTML = value
+    } )
+}
 
-    //logica de cadastro
+function pegaValores(form, valueHandler) {
+    for (let i = 0; i < form.length; i++) {
+        let prop = form[i]
+        switch (prop.type) {
+            case 'text':
+                valueHandler(prop.name, prop.value)
+                break;
+            case 'select-one':
+                valueHandler(prop.name, prop.options[prop.selectedIndex].text)
+                break;
+            case 'radio':
+            case 'checkbox':
+                if (prop.checked) {
+                    valueHandler(prop.name, prop.value)
+                }
+                break;
+        }
+    }
 }
